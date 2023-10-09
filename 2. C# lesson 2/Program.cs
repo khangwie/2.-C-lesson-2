f61724771d43b8140840cac31e21082b00af09bf
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 11:03:07 +0200

    Add project files.

---
+++ 2. C# lesson 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.C__lesson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("          /|");
+            Console.WriteLine("         / |");
+            Console.WriteLine("        /  |");
+            Console.WriteLine("       /   |");
+            Console.WriteLine("      /    |");
+            Console.WriteLine("      ------");
+
+            // freeze console
+            Console.ReadLine();
+        }
+    }
+}
